b23150c7fd655f9774a67e6650a9ee16529a88a2
Ward <1060834@hr.nl>
2024-10-08 12:07:08 +0200

    added named routes to navigation

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/exercises', function () {
+    return view('exercises');
+})->name('exercises');
