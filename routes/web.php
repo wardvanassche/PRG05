5c2c4b2ef28bddb7dcc315c0a55285917fa4affe
Ward <1060834@hr.nl>
2024-10-10 14:10:58 +0200

    controller for homepage

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
