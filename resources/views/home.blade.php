ce07efa257ce5e61b57a7ea7e4ea449d7c655880
Ward <1060834@hr.nl>
2024-10-10 11:47:28 +0200

    navbar fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,23 +13,30 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href=" {{ route('home') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=" {{ route('exercises') }}">Exercises</a>
-                </li>
-            </ul>
-            <div>
+        <div class="row  collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="col">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href=" {{ route('home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=" {{ route('exercises') }}">Exercises</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-dark" type="submit">Search</button>
+                </form>
+            </div>
+            <div class="col d-flex justify-content-end">
                 <a href="{{ route('login') }}" class="btn btn-outline-dark" role="button">Login</a>
                 <a href="{{ route('signup') }}" class="btn btn-outline-dark" role="button">Sign up</a>
             </div>
         </div>
     </div>
 </nav>
-<h1>Welkom!</h1>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
