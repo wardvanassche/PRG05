0eb714d967ef8d5b5e6c30112222dfe335287c45
Ward <1060834@hr.nl>
2024-10-23 16:45:46 +0200

    changed edit profile page layout

---
+++ resources/views/components/back-button.blade.php
@@ -0,0 +1,6 @@
+<div class="flex mx-auto my-3 p-4 sm:p-8">
+    <a href="{{ route('dashboard') }}"
+       class="p-4 bg-gray-800 shadow sm:rounded-lg text-white hover:bg-gray-700">
+        Back
+    </a>
+</div>
