ce5aafc8d05958b6ec7c5f31e2a8b4b7cd3997a4
Ward <1060834@hr.nl>
2024-11-02 13:36:49 +0100

    Changed detail page front end

--- resources/views/exercises/show.blade.php
+++ resources/views/exercises/show.blade.php
@@ -2,7 +2,7 @@
 
     <!-- Back Button -->
     <div class="max-w-3xl mx-auto mt-6">
-        <a href="{{route('exercises.index')}}"
+        <a href="{{ route('exercises.index') }}"
            class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
             ← Back
         </a>
@@ -12,19 +12,19 @@
     <div class="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-10">
 
         <!-- Exercise Image -->
-        <img class="w-full h-48 object-cover" src=" {{ $exercise->image }}"
+        <img class="w-full h-auto object-cover rounded-lg" src="{{ $exercise->image }}"
              alt="Picture of {{ $exercise->name }}">
 
         <!-- Exercise Title -->
-        <h1 class="text-3xl font-bold text-gray-800 mb-2">{{$exercise->name}}</h1>
-
-        <!-- Exercise Description -->
-        <p class="text-gray-600 mb-4">
-            {{$exercise->description}}
-        </p>
+        <h1 class="text-3xl font-bold text-gray-800 mb-2 mt-4">{{ $exercise->name }}</h1>
 
         <!-- Exercise Category -->
         <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ $exercise->category->name }}</span>
+
+        <!-- Exercise Description -->
+        <p class="text-gray-600 mb-4 pt-2">
+            {{$exercise->description}}
+        </p>
     </div>
 </x-app-layout>
