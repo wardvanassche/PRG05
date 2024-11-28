ad38e5bc11010670661fa315aed0bfc1363a1f2a
Ward <1060834@hr.nl>
2024-11-28 16:39:11 +0100

    Removed unnessecary functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,29 +16,6 @@ class UserController extends Controller
         return view('users.index', ['users' => User::all()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
